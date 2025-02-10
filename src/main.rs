cd0ac1470139b8f3030bc0cdd883593d7c31969c
Trevor Kimutai <Trv6775@gmail.com>
2025-02-10 05:35:02 +0300

    So far I have covered Scalar types which represents single values and compound types which groups multiple values into one type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let a=[1,2,3,4,5];
+    println!("Please enter an index array!");
+    let mut index=String::new();
+    io::stdin().read_line(& mut index).expect("Failed to read line");
+    let index:usize=index.trim().parse().expect("Index should be a number");
+    let element=a[index];
+    println!("The value of element at index {index} is {element}");
 }
