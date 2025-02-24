0885a51c224f3a01904ab4c1dae6fe89fe7c9dfd
Trevor Kimutai <Trv6775@gmail.com>
2025-02-24 05:08:30 +0300

    Here we are introduced to parameters which are special variables tha t are a part of the function signature, NOTE: Arguments are the concrete values provided for parameters

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use std::io;
-
 fn main() {
-    let a=[1,2,3,4,5];
-    println!("Please enter an index array!");
-    let mut index=String::new();
-    io::stdin().read_line(& mut index).expect("Failed to read line");
-    let index:usize=index.trim().parse().expect("Index should be a number");
-    let element=a[index];
-    println!("The value of element at index {index} is {element}");
+   another_function(5);
+    println!("Here lies the main function");
+}
+fn another_function(x:i32) {
+    println!("The value of x is {x}");
 }
