ab52b3f9aecf90f3973d71deaedd33f51abc5087
Trevor Kimutai <Trv6775@gmail.com>
2025-02-27 14:54:24 +0300

    NOTE: The above code records both the index and element but heres the funny thing if we place index above the println the indexing would match the array elements in that it would match the counting starting from 1 instead of 0 but once we drop the index after the println everything now works well this I have observed with loops, basically the positioning of count is important

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
  let a=[10,20,30,40,50];
  let mut index=0;
- while index<5{
-   println!("the value is: {} at index {index}",a[index]);
+for element in a{
+   println!("The value at index {} is: {element}",index);
    index+=1;
- }
+}
 }
