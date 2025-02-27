c2e96a720c4d5c4d90350239c3464fd6371231f7
Trevor Kimutai <Trv6775@gmail.com>
2025-02-27 13:34:54 +0300

    Here we are learning about Control Flows and for this example I decided to show how if statements and match expressions are similar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-   let x=(let y=6);
+   let number=3;
+   // if number>5{
+   //    println!("Condition was false");
+   // }else {
+   //     println!("Condition was true");
+   // }
+   match number<5 {
+      true=>println!("Condition was true"),
+      false=>println!("COndition was false")
+   }
 }
