628eddc6e07f344590004370a1925b9d2dde0438
Trevor Kimutai <Trv6775@gmail.com>
2025-02-27 14:44:16 +0300

    Using while loop to count up through elements in an array. It is okay but error prone as if we change the array to have 4 elements then forget to update the condition to index < 4 the code would panic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-let mut number=3;
-while number!=0{
-   println!("{number}");
-   number-=1;
-}
+ let a=[10,20,30,40,50];
+ let mut index=0;
+ while index<5{
+   println!("the value is: {} at index {index}",a[index]);
+   index+=1;
+ }
 }
