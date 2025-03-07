ba7e9b6b55531b6a861281650e09323e2dd22d04
Trevor Kimutai <Trv6775@gmail.com>
2025-03-07 06:08:51 +0300

    This code compiles well, but we want to use a reference

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
    let  s1=String::from("Hello");
-   let s2=s1;
-   println!("{s1}");
+   let(s2,len)=calculate_len(s1);
+   println!("The length of {s2} is {len}");
+}
+fn calculate_len(s:String)->(String,usize){
+   let length=s.len();
+   (s,length)
 }
