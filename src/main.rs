8dd4d5e95806144c9753412880849f0f1ebb0b59
Trevor Kimutai <Trv6775@gmail.com>
2025-03-23 10:27:25 +0300

    In order to make a Struct mutable we have to make the whole struct mutable

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ struct User{
    sign_in_count:u64
 }
 fn main(){
-   let user1=User{
+   let mut user1=User{
       active:true,
       username:String::from("Trevor Kimutai"),
       email:String::from("trv6775@gmail.com"),
       sign_in_count:1
    };
+   user1.email=String::from("trv6775330@gmail.com");
    println!("{:?}",user1);
 }
