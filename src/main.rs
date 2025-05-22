306b02c1e94f04de576648d921d28df887565596
Trevor Kimutai <Trv6775@gmail.com>
2025-05-22 06:18:39 +0300

    Now this is the second way of destructuring a tuple by use of the period (.)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main(){
     let tup=(1,'a',0.1,"Hi");
-    let (a,b,c,d)=tup;
-    println!("The value of a is {}, b is {}, c is {} and d id {}",a,b,c,d);
+    let int=tup.0;
+    let char=tup.1;
+    let float=tup.2;
+    let string=tup.3;
+    println!("int is {},char is {},float is {},string is {} ",int,char,float,string);
 }
